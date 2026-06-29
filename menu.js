9317e55bb337858f7f4ed132ff891d0d8117d3c3
Shinatshi <44529279+Shinatshi@users.noreply.github.com>
2026-06-29 13:59:38 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -4,3 +4,15 @@ Toggle.addEventListener('click',function() {
     console.log(1);
     List.classList.toggle('active');
 })
+let links = document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function(element){
+    element.addEventListener('click',function(event){
+        event.preventDefault();
+        targetID = element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView({
+            behavior: 'smooth',
+            block:'start'
+        })
+    })
+})
